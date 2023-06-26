817c845c67360175eee3f8bbadb5171423dd19d9
Abel Galindo Pascual <abel.gp.1991@hotmail.es>
2023-06-26 12:52:03 +0200

    Ver Tareas

--- menu/opcions/llistarTasques.php
+++ menu/opcions/llistarTasques.php
@@ -1,19 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>
-        Aqui se muestran todas las tareas desde PHP y la base de datos
-    </p>
 
-<form action="">
-    <p>Busca el usser, id, titulo</p>
-    <input type="text" name="" id="">
-</form>
+<?php
 
-</body>
-</html>
+session_start();
+$id = $_SESSION['idUsuario'];
+
+$mysql = new mysqli("localhost","root","","to_do");
+
+    if ($mysql->connect_error){
+
+        die("Problemas con la conexión a la base de datos");
+
+    }
+
+    else {
+
+        echo "Se ha conectado satisfactoriamente a la BDD" . "<br>" . "<br>";
+
+    }
+
+$sql = "SELECT tasques.idTasques,tasques.nom_tasques FROM to_do.tasques WHERE tasques.Usuario_idUsuario LIKE '$id'" 
+            or die($mysql->error);
+
+$tasques = $mysql->query($sql);
+
+echo '<table class = "tablalistado">';
+echo '<tr><th>Código</th><th>Descripción</th></tr>';
+
+while ($resultado = mysqli_fetch_array($tasques)){
+
+    echo '<tr>';
+    echo '<td>';
+    echo $resultado['idTasques'];
+    echo '</td>';
+    echo '<td>';
+    echo $resultado['nom_tasques'];
+    echo '</td>';
+    echo '</tr>';
+
+}
+
+echo '<table>';
+
+$mysql->close();
+
+?>
+    
+    
